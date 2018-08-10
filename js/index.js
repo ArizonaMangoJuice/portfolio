1177fd012c24e1f8659d569908e7e326aa3e50b0
Isael Lizama <isaelwebdeveloper@gmail.com>
2018-08-10 13:53:38 -0600

    Updates

--- js/index.js
+++ js/index.js
@@ -1,40 +1,24 @@
 'use strict';
-
-function standardize(name){
+let name;
+function standardize(name) {
   name = name.replace(/\s/g, '').toLowerCase();
-  console.log(name);
   return name;
 }
 
-function openSection(sectionName){
+function openSection(sectionName) {
   let name = standardize(sectionName);
 
   $('.section').css('display', 'none');
-    
-  if ( $(window).width() > 610 && name === 'projects') {    
+
+  if ($(window).width() > 610 && name === 'projects') {
     console.log($(`#${name}`).children());
-    if(name === 'projects'){
+    if (name === 'projects') {
       $(`#${name}`).css('display', 'flex');
       $(`#${name}`)
         .addClass('.grid-projects');
     } else {
       $(`#${name}`).css('display', 'block');
-    } 
-    
-    $(window).on('resize', function(){
-      var win = $(this); //this = window
-      if (win.height() >= 610) { 
-        if(name === 'projects'){
-          $(`#${name}`).css('display', 'flex');
-          $(`#${name}`)
-            .addClass('.grid-projects');
-        } else {
-          $(`#${name}`).css('display', 'block');
-        } 
-      }
-      // if (win.width() >= 1280) { /* ... */ }
-    });
-
+    }
   } else {
 
     $(`#${name}`).css('display', 'block');
@@ -42,17 +26,11 @@ function openSection(sectionName){
   }
 }
 
-function main (){
-  //under construction!!
-  // $(window).on('load',function() {
-  //   $('body').removeClass('preload');
-  // });
-
-  $('.nav-button').click(function() {
+function main() {
+  $('.nav-button').click(function () {
     let sectionName = $(this).html();
     $('.nav-button').removeClass('.nav-focus');
     $(this).addClass('.nav-focus');
-    // console.log($(this));
     openSection(sectionName);
   });
 }
