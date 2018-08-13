626960f0e8b0e2ba56ca9517825b5c06e34c98a5
Isael Lizama <isaelwebdeveloper@gmail.com>
2018-08-13 14:46:59 -0600

    updated css and made nav consistent

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,7 +1,7 @@
 'use strict';
 let name;
-function standardize(name) {
-  name = name.replace(/\s/g, '').toLowerCase();
+function standardize(navname) {
+  name = navname.replace(/\s/g, '').toLowerCase();
   return name;
 }
 
@@ -11,10 +11,13 @@ function openSection(sectionName) {
   
   $('.section').css('display', 'none');
 
-  if ($(window).width() > 610 && name === 'projects') {
+  if ($(window).width() > 610 || name === 'projects') {
     if (name === 'projects') {
       $(`#${name}`)
-        .css('display', 'flex');
+        .css({
+          'display': 'flex',
+          'align-items': 'center'
+        });
       $(`#${name}`)
         .addClass('.grid-projects');
     } else {
@@ -22,14 +25,36 @@ function openSection(sectionName) {
         .css('display', 'block');
     }
   } else {
-
     $(`#${name}`)
       .css('display', 'block');
-
   }
+  
 }
 
 function main() {
+  $(window).on('resize', function(){
+    var win = $(this); 
+    if(name){
+      if (win.width() > 599) {  
+        if (name === 'projects') {
+          $(`#${name}`)
+            .css('display', 'flex');
+          $(`#${name}`)
+            .addClass('.grid-projects');
+        }
+      }else {
+        console.log(name);
+        if(name === 'projects'){
+          $(`#${name}`)
+            .removeClass('.grid-projects');
+          $('.projects').css({
+            'display': 'flex',
+            'align-items': 'center'
+          });
+        }
+      }
+    }
+  });
   $('.nav-button').click(function () {
     let sectionName = $(this).html();
     $('.nav-button').removeClass('.nav-focus');
